c54b60ac33494bb6ff6c89ae3b2e57b0e2d5e5fe
Than Htut Aung <rday61062@gmail.com>
2024-02-25 16:45:40 +0630

    added: tracking

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,6 +5,19 @@ import PageLoading from "./page-loading";
 function App() {
    const [loading, setLoading] = useState(true);
 
+   // dummy visitors tracking with firebase realtime database
+   useEffect(() => {
+      const mutateVisitors = async () =>
+         await fetch("https://tracker-9644a-default-rtdb.firebaseio.com/clicks.json", {
+            method: "POST",
+            headers: {
+               "Content-type": "application/json",
+            },
+            body: JSON.stringify({ date: new Date() }),
+         });
+      mutateVisitors();
+   }, []);
+
    useEffect(() => {
       const timeout = setTimeout(() => {
          setLoading(false);
--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+
 root.render(
    <React.StrictMode>
       <App />
